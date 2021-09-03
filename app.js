083579bf751da42e9638e22048a8be0dbeedbda3
lufeee <contact@lufe.jp>
2021-09-03 16:35:17 +0900

    done

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
+
+app.use(express.static(path.join(__dirname + '/src')))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/src/index.html')
--- src/fingerprint.js
+++ src/fingerprint.js
@@ -1,65 +1,42 @@
-var fingerprint = function (options) {
+function getLanguage() {
+    return navigator.language;
+}
 
-    getLangage: function() {
-        return navigator.language;
-    }
+function getOS() {
+    return navigator.platform;
+}
 
-    getOS: function() {
-        return navigator.platform;
-    }
+function getCpuThread() {
+    return navigator.hardwareConcurrency;
+}
 
-    getNetworkInfomation: function() {
-        return navigator.connection;
-    }
+function getBrowserType() {
+    var browser = navigator.userAgent;
+    return browser;
+}
 
-    getCpuThread: function() {
-        return navigator.hardwareConcurrency;
-    }
+function getTimezone() {
+    return new Date().getTimezoneOffset();
+}
 
-    getBrowserType: function() {
-        var browser = navigator.userAgent;
-        return browser;
-    }
+function getScreenResolution() {
+    return (screen.height > screen.width) ? [screen.height, screen.width] : [screen.width, screen.height];
+}
 
-    getTimezone: function() {
-        return new Date().getTimezoneOffset();
-    }
+function getCanvasFingerprint() {
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
 
-    getPluginsString: function() {
-        return this.map(navigator.plugins, function(p) {
-            var mimeTypes = this.map(p. function(mt) {
-                return [mt.type, mt.suffixes].join('~');
-            }).join(',');
-            return [p.name, p.description, mimeTypes].join('::');
-        }, this).join(';');
-    }
+    var txt = 'CANVAS_FINGERPRINT';
+    ctx.textBaseline = 'top';
+    ctx.font = "14px 'Arial'";
+    ctx.textBaseline = 'alphabetic';
+    ctx.fillStyle = "#f60";
+    ctx.fillRect(125, 1, 62, 20);
+    ctx.fillStyle = "#069";
+    ctx.fillText(txt, 2, 15);
+    ctx.fillStyle = "rgba(102, 204, 0, 0.7)";
+    ctx.fillText(txt, 4, 17);
+    return canvas.toDataURL();
+}
 
-    getScreenResolution: function() {
-        return (screen.height > screen.width) ? [screen.height, screen.width] : [screen.width, screen.height];
-    }
-
-    isCanvasSupported: function() {
-        var element = document.createElement('canvas');
-        return !!(element.getContext && element.getContext('2d'));
-    }
-
-    getCanvasFingerprint: function() {
-        var canvas = document.createElement('canvas');
-        var ctx = canvas.getContext('2d');
-        if(isCanvasSupported()){
-            return null;
-        }
-
-        var txt = 'CANVAS_FINGERPRINT';
-        ctx.textBaseline = 'top';
-        ctx.font = "14px 'Arial'";
-        ctx.textBaseline = 'alphabetic';
-        ctx.fillStyle = "#f60";
-        ctx.fillRect(125, 1, 62, 20);
-        ctx.fillStyle = "#069";
-        ctx.fillText(txt, 2, 15);
-        ctx.fillStyle = "rgba(102, 204, 0, 0.7)";
-        ctx.fillText(txt, 4, 17);
-        return canvas.toDataURL();
-    }
-};
